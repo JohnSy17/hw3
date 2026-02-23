a0612ecccd272ee29f1fc3e6f9a432ac18c5bd39
JohnSy17 <85195933+JohnSy17@users.noreply.github.com>
2026-02-23 18:12:50 +0000

    Finish places controller and started entries build

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,14 @@
 class EntriesController < ApplicationController
+  def index
+    # show all of the posts that are relevant to the city selected
+    @entries_rend = Entry.find_by({"place_id" => params["id"]})
+  end
+
+  def new
+    # render the new posts for a certain location
+  end
+
+  def create
+    #allow for creation of a new entry
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,12 @@ class PlacesController < ApplicationController
     # render places/new view new place form
   end
 
+  def show
+    # capture information that will then re-direct to the appropriate entries page
+    @place_name = Place.find_by({"id" => params["id"]})["name"]
+    redirect_to "/entries/"
+  end
+
   #Creates a new place that can be added to the list
   def create
     @place_new = Place.new( 
